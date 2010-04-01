d841fc2f97febbee7afae17f8248d2d492194390
Michael Jackson <mjijackson@gmail.com>
2010-04-01 17:37:38 -0600

    whoops, forgot to require rack :]

--- lib/rack/accept.rb
+++ lib/rack/accept.rb
@@ -1,7 +1,9 @@
+require 'rack'
+
 module Rack::Accept
 
   # The current version of rack-accept.
-  VERSION = [0, 1]
+  VERSION = [0, 1, 1]
 
   # Returns the current version of rack-accept as a string.
   def self.version

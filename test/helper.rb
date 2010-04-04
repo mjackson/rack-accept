77b9abfe43dcd736a390aaad2910d5840845bcd8
Michael Jackson <mjijackson@gmail.com>
2010-04-03 22:12:02 -0600

    added tests for Rack::Accept::Context

--- test/helper.rb
+++ test/helper.rb
@@ -15,3 +15,24 @@ $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
 require 'test/unit'
 require 'rack/accept'
+
+class Test::Unit::TestCase
+  attr_reader :context
+  attr_reader :response
+
+  def status
+    @response && @response.status
+  end
+
+  def request(env={}, method='GET', uri='/')
+    @context = Rack::Accept.new(fake_app)
+    yield @context if block_given?
+    mock_request = Rack::MockRequest.new(@context)
+    @response = mock_request.request(method.to_s.upcase, uri, env)
+    @response
+  end
+
+  def fake_app(status=200, headers={}, body=[])
+    lambda {|env| Rack::Response.new(body, status, headers).finish }
+  end
+end
